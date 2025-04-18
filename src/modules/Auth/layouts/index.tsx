92c0fdc19fbb79d6c5628cb1c62dec8fecdc41d3
abdalghani0 <117361839+abdalghani0@users.noreply.github.com>
2025-04-18 16:46:04 +0300

    feat: - Add LangaugeSelect component to auth layout

--- src/modules/Auth/layouts/index.tsx
+++ src/modules/Auth/layouts/index.tsx
@@ -1,12 +1,16 @@
 import AuthImage from "@/common/assets/images/login/image.svg";
 import AuthBackground from "@/common/assets/images/login/background.svg";
 import { Outlet } from "react-router-dom";
+import LanguageSelect from "@/common/components/Translation/LanguageSelect";
 
 export default function AuthLayout() {
   return (
     <div className="flex w-full h-screen">
       <div className="relative lg:p-16 flex justify-center items-center">
         <img src={AuthImage} />
+        <div className={`absolute top-4 left-4`}>
+          <LanguageSelect />
+        </div>
       </div>
       <div className={`relative h-full w-full flex-grow`}>
         <img
